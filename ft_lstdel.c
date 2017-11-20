31466f68872368143e20d634e204f0cb26a6a92f
Chanèze Aroua <chanezearoua@macbook-cha.home>
2017-11-20 17:00:00 +0100

    bonus

---
+++ ft_lstdel.c
@@ -0,0 +1,13 @@
+
+
+#include <stdlib.h>
+#include "libft.h"
+
+void	ft_lstdel(t_list **alst, void (*del)(void *, size_t))
+{
+	if (*alst && del)
+	{
+		ft_lstdel(&((*alst)->next), del);
+		ft_lstdelone(alst, del);
+	}
+}
---
+++ ft_lstdelone.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <string.h>
+#include "libft.h"
+
+void	ft_lstdelone(t_list **alst, void (*del)(void *, size_t))
+{
+	if (*alst)
+	{
+		(*del)((*alst)->content, (*alst)->content_size);
+	}
+	ft_memdel((void **)alst);
+}
